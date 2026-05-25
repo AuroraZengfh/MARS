1a5c2b00039e69872a0edbbf2ebecde6b352ee52
AuroraZengfh <119220964+AuroraZengfh@users.noreply.github.com>
2026-05-25 20:53:43 +0800

    Update qwen_inference.sh

--- evaluation/qwen_inference.sh
+++ evaluation/qwen_inference.sh
@@ -2,5 +2,7 @@ MODEL_PATH=$1
 QUESTION_PATH=$2
 OUTPUT_PATH=$3
 
+mkdir -p ${OUTPUT_PATH}
+
 python evaluation/qwen_inference.py --model_path ${MODEL_PATH} --question_path ${QUESTION_PATH} --output_path ${OUTPUT_PATH}
 
